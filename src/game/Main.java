10ba1bcb452d092e59a963c915cc97862a5c1799
XboxPro39 <57122545+XboxPro39@users.noreply.github.com>
2020-02-05 20:22:52 -0600

    Add 2 Classes and Quad

--- src/game/Main.java
+++ src/game/Main.java
@@ -4,14 +4,34 @@ import org.lwjgl.Sys;
 import org.lwjgl.opengl.Display;
 
 import engine.DisplayManager;
+import engine.Renderer;
+import models.RawModel;
+import util.Loader;
 
 public class Main {
 	public static void main(String[] args) {
 		DisplayManager.createDisplay();
 		
+		Loader loader = new Loader();
+		Renderer renderer = new Renderer();
+		
+		float[] vertices = {
+				-0.5f, 0.5f, 0f,
+			    -0.5f, -0.5f, 0f,
+			    0.5f, -0.5f, 0f,
+			    0.5f, -0.5f, 0f,
+			    0.5f, 0.5f, 0f,
+			    -0.5f, 0.5f, 0f
+		};
+		
+		RawModel model = loader.loadToVAO(vertices);
+		
 		System.out.println("LWJGL Version " + Sys.getVersion());
 		
 		while(!Display.isCloseRequested()) {
+			renderer.prepare();
+			renderer.render(model);
+			
 			DisplayManager.updateDisplay();
 		}
 		DisplayManager.closeDisplay();
