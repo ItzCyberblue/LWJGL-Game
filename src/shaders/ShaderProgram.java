fdf4745f76585005b6629f7ba05bab72f0bda7d4
XboxPro39 <57122545+XboxPro39@users.noreply.github.com>
2020-02-07 18:27:05 -0600

    Update ShaderProgram.java

--- src/shaders/ShaderProgram.java
+++ src/shaders/ShaderProgram.java
@@ -65,7 +65,7 @@ public abstract class ShaderProgram {
 		GL20.glCompileShader(shaderID);
 		if(GL20.glGetShaderi(shaderID, GL20.GL_COMPILE_STATUS) == GL11.GL_FALSE) {
 			System.out.println(GL20.glGetShaderInfoLog(shaderID, 500));
-			System.err.println("Failed to complies shader files.");
+			System.err.println("Failed to compile shader files.");
 			System.exit(-1);
 		}
 		return shaderID;
