ee64d1070ea62cc29cb5ea6db8d0df77b9686d9c
XboxPro39 <57122545+XboxPro39@users.noreply.github.com>
2020-02-09 22:57:31 -0600

    Update Loader.java

--- src/util/Loader.java
+++ src/util/Loader.java
@@ -35,7 +35,7 @@ public class Loader {
 	public int loadTexture(String fileName) {
 		Texture texture = null;
 		try {
-			texture = TextureLoader.getTexture("PNG", new FileInputStream("res/" + fileName + ".png"));
+			texture = TextureLoader.getTexture("PNG", new FileInputStream("res/" + fileName + ".PNG"));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
