ee573f7c8cf5d1cfa94412f59c4745283e05c58c
XboxPro39 <57122545+XboxPro39@users.noreply.github.com>
2020-02-09 23:17:43 -0600

    Added stuff

--- src/shaders/StaticShader.java
+++ src/shaders/StaticShader.java
@@ -1,8 +1,12 @@
 package shaders;
 
+import org.lwjgl.util.vector.Matrix4f;
+
 public class StaticShader extends ShaderProgram {
 	private static final String VERTEX_FILE = "src/shaders/vertexShader.txt";
 	private static final String FRAGMENT_FILE = "src/shaders/fragmentShader.txt";
+	
+	private int location_transformMatrix;
 	
 	public StaticShader() {
 		super(VERTEX_FILE, FRAGMENT_FILE);
@@ -12,6 +16,15 @@ public class StaticShader extends ShaderProgram {
 	protected void bindAttributes() {
 		super.bindAttribute(0, "position");
 		super.bindAttribute(1, "textureCoords");
+	}
+
+	@Override
+	protected void getAllUniformLocations() {
+		location_transformMatrix = super.getUniformLocation("transformMatrix");
+	}
+	
+	public void loadTransformMatrix(Matrix4f matrix) {
+		super.loadMatrix(location_transformMatrix, matrix);
 	}
 	
 }
